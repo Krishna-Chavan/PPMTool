3ad40b0e0e8af3f53d8697ae5d399df5f7a02742
krishna.chavan <krishna.chavan@brillio.com>
2021-10-22 15:24:59 +0530

    added validation

--- PPMTool/src/main/java/com/brillio/PPMTool/controller/ProjectController.java
+++ PPMTool/src/main/java/com/brillio/PPMTool/controller/ProjectController.java
@@ -1,14 +1,21 @@
 package com.brillio.PPMTool.controller;
 
 import com.brillio.PPMTool.domain.Project;
+import com.brillio.PPMTool.service.MapValidationErrorService;
 import com.brillio.PPMTool.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -17,8 +24,15 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if (errorMap != null) return errorMap;
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
     }
