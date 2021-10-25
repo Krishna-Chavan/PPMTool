746e8edb2386b8761b82cd74b58d4efa1acd7317
krishna.chavan <krishna.chavan@brillio.com>
2021-10-25 14:28:28 +0530

    find all projects

--- PPMTool/src/main/java/com/brillio/PPMTool/service/ProjectService.java
+++ PPMTool/src/main/java/com/brillio/PPMTool/service/ProjectService.java
@@ -21,4 +21,17 @@ public class ProjectService {
             throw new ProjectIdException("Project Id '"+project.getProjectIdentifier().toUpperCase()+"' Already Exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null){
+            throw new ProjectIdException("Project Id '"+projectId+"' does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
 }
